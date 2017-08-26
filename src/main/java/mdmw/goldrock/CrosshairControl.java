7cfadb92d5f50d7fe2824f11fe23ecc734576a01
Ben Tucker <nenofite@gmail.com>
2017-08-26 07:59:09 -0700

    Format CrosshairControl

--- src/main/java/mdmw/goldrock/CrosshairControl.java
+++ src/main/java/mdmw/goldrock/CrosshairControl.java
@@ -11,14 +11,16 @@ import com.jme3.ui.Picture;
 /**
  * Make the crosshair follow the mouse cursor. On click, send a shoot event.
  */
-public class CrosshairControl extends AbstractControl {
+public class CrosshairControl extends AbstractControl
+{
 
     /**
      * Make a crosshair sprite and attach a new control to it.
      *
      * @return A new crosshair with a controller on it
      */
-    public static Spatial makeCrosshair(Application app) {
+    public static Spatial makeCrosshair(Application app)
+    {
 
         Picture crosshair = new Picture("Arrow");
         crosshair.setImage(app.getAssetManager(), "Sprites/crosshairs.png", true);
@@ -32,12 +34,14 @@ public class CrosshairControl extends AbstractControl {
     }
 
     @Override
-    protected void controlUpdate(float tpf) {
+    protected void controlUpdate(float tpf)
+    {
 
     }
 
     @Override
-    protected void controlRender(RenderManager rm, ViewPort vp) {
+    protected void controlRender(RenderManager rm, ViewPort vp)
+    {
 
     }
 }
