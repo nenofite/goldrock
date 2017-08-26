0840415a0d050e191c6f660ec688d3551d371872
Ben Tucker <nenofite@gmail.com>
2017-08-26 07:56:38 -0700

    Format main

--- src/main/java/mdmw/goldrock/Main.java
+++ src/main/java/mdmw/goldrock/Main.java
@@ -5,27 +5,27 @@ import com.jme3.system.AppSettings;
 
 public class Main extends SimpleApplication {
 
-        public static void main(String[] args){
-            new Main().start();
-        }
-
-        public Main() {
-            AppSettings settings = new AppSettings(true);
-            settings.setVSync(true);
-            settings.setFrameRate(100);
-            setSettings(settings);
-        }
-
-        @Override
-        public void simpleInitApp() {
-            setDisplayStatView(false);
-
-            ShootDeerState shootDeerState = new ShootDeerState();
-            stateManager.attach(shootDeerState);
-        }
-
-        @Override
-        public void simpleUpdate(float tpf) {
-            super.simpleUpdate(tpf);
-        }
+    public static void main(String[] args) {
+        new Main().start();
     }
+
+    public Main() {
+        AppSettings settings = new AppSettings(true);
+        settings.setVSync(true);
+        settings.setFrameRate(100);
+        setSettings(settings);
+    }
+
+    @Override
+    public void simpleInitApp() {
+        setDisplayStatView(false);
+
+        ShootDeerState shootDeerState = new ShootDeerState();
+        stateManager.attach(shootDeerState);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+    }
+}
