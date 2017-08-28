8161036a4da09bcddb2cc9aef636969d575b2829
Ben Tucker <nenofite@gmail.com>
2017-08-27 18:48:08 -0700

    Spin wolf head

--- src/main/java/mdmw/goldrock/WolfEatsYouControl.java
+++ src/main/java/mdmw/goldrock/WolfEatsYouControl.java
@@ -2,6 +2,9 @@ package mdmw.goldrock;
 
 import com.jme3.audio.AudioData;
 import com.jme3.audio.AudioNode;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
@@ -13,37 +16,34 @@ public class WolfEatsYouControl extends AbstractControl
 {
     private static final float WIDTH_PORTION = 0.9f;
     private static final float HEIGHT_PORTION = 0.9f;
-    private static final float MOVE_SPEED = 1000;
-    private float width;
-    private float height;
+    private static final long ENTRY_DURATION = 500;
+    private long startedEntry;
     private Main app;
     private AudioNode biteSound;
 
     public static Spatial createWolfEatingYou(Main app)
     {
         Node base = new Node("you have met");
-        Picture p = new Picture("a terrible fate");
+        Picture picture = new Picture("a terrible fate");
         // haven't you
 
-        p.setImage(app.getAssetManager(), "Sprites/git_munched.png", true);
+        picture.setImage(app.getAssetManager(), "Sprites/git_munched.png", true);
         float width = app.getCamera().getWidth() * WIDTH_PORTION;
         float height = app.getCamera().getHeight() * HEIGHT_PORTION;
-        p.setWidth(app.getCamera().getWidth() * WIDTH_PORTION);
-        p.setHeight(app.getCamera().getHeight() * HEIGHT_PORTION);
+        picture.setWidth(width);
+        picture.setHeight(height);
+        picture.setLocalTranslation(-width / 2, -height / 2, 0);
 
-        base.attachChild(p);
-        base.addControl(new WolfEatsYouControl(app, width, height));
-
-        base.setLocalTranslation(app.getCamera().getWidth() / 2 - width / 2, -height, 5);
+        base.attachChild(picture);
+        base.addControl(new WolfEatsYouControl(app));
+        base.setLocalTranslation(app.getCamera().getWidth() / 2, app.getCamera().getHeight() / 2, 5);
 
         return base;
     }
 
-    public WolfEatsYouControl(Main app, float width, float height)
+    public WolfEatsYouControl(Main app)
     {
         this.app = app;
-        this.width = width;
-        this.height = height;
 
         biteSound = new AudioNode(app.getAssetManager(), "Audio/mdmw_bite.wav", AudioData.DataType.Buffer);
         biteSound.setPositional(false);
@@ -57,21 +57,33 @@ public class WolfEatsYouControl extends AbstractControl
         if (spatial != null)
         {
             biteSound.playInstance();
+            startedEntry = System.currentTimeMillis();
         } else
         {
-
         }
     }
 
     @Override
     protected void controlUpdate(float tpf)
     {
-        float destY = app.getCamera().getHeight() / 2 - height / 2;
-        float currentY = getSpatial().getLocalTranslation().getY();
+        float frac = (float) (System.currentTimeMillis() - startedEntry) / ENTRY_DURATION;
 
-        float moveY = Math.min(destY - currentY, MOVE_SPEED * tpf);
+        if (frac >= 1)
+        {
+            // Reset rotation
+            getSpatial().setLocalRotation(Quaternion.IDENTITY);
+            // Set to full scale
+            getSpatial().setLocalScale(1);
+        } else
+        {
+            // Spin
+            float angle = FastMath.interpolateLinear(frac, 0, 2 * FastMath.PI);
+            getSpatial().setLocalRotation(new Quaternion().fromAngleAxis(angle, Vector3f.UNIT_Z));
 
-        getSpatial().move(0, moveY, 0);
+            // Grow
+            float scale = FastMath.interpolateLinear(frac, 0, 1);
+            getSpatial().setLocalScale(scale);
+        }
     }
 
     @Override
